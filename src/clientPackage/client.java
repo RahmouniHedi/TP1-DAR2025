11e854917d28bc8ab0f2b6eaf509c5aca455abcd
Rahmouni Mohamed Hedi <rahmounimohamedhedi@fss.u-sfax.tn>
2025-10-05 22:37:34 +0100

    Changez le nom du localhost par une adresse IP.

--- src/clientPackage/client.java
+++ src/clientPackage/client.java
@@ -6,7 +6,7 @@ public class client {
         // La première étape :
 
         System.out.println("Je suis un client pas encore connecté...");
-        Socket clientSocket = new Socket("localhost", 1234);
+        Socket clientSocket = new Socket("127.0.0.1", 1234);
         System.out.println("je suis un client connecté");
         // La dernière étape : Fermer socket
         clientSocket.close();
