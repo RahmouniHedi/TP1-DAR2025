c90552573724c9f7339a11dfeb2fba689fb21cab
Rahmouni Mohamed Hedi <rahmounimohamedhedi@fss.u-sfax.tn>
2025-10-12 23:00:01 +0100

    Update Server.java

--- src/serverPackage/Server.java
+++ src/serverPackage/Server.java
@@ -1,37 +1,31 @@
 package serverPackage;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 
 public class Server {
-    static void main() throws IOException {
-        // La première étape :
-        ServerSocket serverSocket;
-        try {
-            serverSocket = new ServerSocket(1234);
-            System.out.println("Je suis un serveur en attente la connexion d'un client ");
 
-            Socket clientsocket = serverSocket.accept();
-            // La deuxième étape :
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(1234)) {
+            System.out.println("Server is waiting for a client connection on port 1234...");
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("A client has connected!");
 
+            InputStream inputStream = clientSocket.getInputStream();
+            OutputStream outputStream = clientSocket.getOutputStream();
 
-            System.out.println("un client est connecté");
-            InputStream in = clientsocket.getInputStream();
-            DataInputStream dis = new DataInputStream(in);
-            int nbr = dis.readInt();
-            int resp = nbr*5;
-            OutputStream os = clientsocket.getOutputStream();
-            DataOutputStream dos = new DataOutputStream(os);
-            dos.writeInt(resp);
-// La dernière étape : Fermer socket
-        clientsocket.close();
-        serverSocket.close();
-        dis.close();
-        dos.close();
+            int numberFromClient = inputStream.read();
+            System.out.println("Received number from client: " + numberFromClient);
+
+            int result = numberFromClient * 2;
+            System.out.println("Calculation result: " + result);
+
+            outputStream.write(result);
+            System.out.println("Sent result back to the client.");
+
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.err.println("An error occurred in the server: " + e.getMessage());
+            e.printStackTrace();
         }
-
-    }}
+    }
+}
